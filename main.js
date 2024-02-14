2b8c7d5a0d2b074a0e69de85b434f3709866476b
miyagi-h-tech <mhkawsdd1027@gmail.com>
2024-02-14 16:50:28 +0900

    JavaScriptのif文 その1 #10

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 
 // if (条件) {
 //   条件が真なら実行
+// } else {
+  // 条件が偽なら実行
 // }
 
-const height = '90';
+const height = 90;
 
-console.log(typeof height);
+//console.log(typeof height);
 
-if (height === 90) {
+if (height >= 91) {
   console.log('身長は' + height + 'cmです。');
+}
+
+if (height <= 90) {
+  console.log('身長は90cm以下です。');
 }
 
 // == 一致
 // === 型も一致
+
+const signal = 'blue'
+
+if (signal === 'red') {
+  console.log('止まれ');
+} else if (signal === 'yellow') {
+  console.log('一旦停止');
+} else {
+  console.log('進む');
+}
+
+const speed = 60;
+
+if (signal === 'blue') {
+  if (speed >= 60) { //ネスト
+    console.log('スピード違反');
+  }
+}
