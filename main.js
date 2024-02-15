4f023cebbc8c15fdd860b7f038c04e9fa0ac36c7
miyagi-h-tech <mhkawsdd1027@gmail.com>
2024-02-15 18:39:13 +0900

    JavaScriptのユーザー定義関数 その2

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
-function test(引数1, 引数2) {
-  //処理
+// インプット引数なし
+// アウトプットなし
 
-  //戻り値
-  return 戻り値;
+function test() {
+  console.log('テスト');
 }
+
+test();
+
+// インプット引数あり
+// アウトプット戻り値なし
+
+const comment = 'コメント';
+const comment_2 = 'コメント2';
+
+function getComment(string) {
+  console.log(string);
+}
+
+getComment(comment_2);
+
+// インプット引数なし
+// アウトプット戻り値あり
+
+function getNumberOfComment() {
+  return 5;
+}
+
+console.log(getNumberOfComment());
+
+const NumberOfComment = getNumberOfComment();
+
+console.log(NumberOfComment);
+
+// インプット 2つ
+// アウトプットもあり
+
+function sumPrice(int1, int2) {
+  let int3 = int1 + int2;
+  return int3;
+}
+
+const total = sumPrice(1, 2);
+console.log(total);
