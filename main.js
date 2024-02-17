ea930008724e16aae2820d4869c6936e9513fb92
miyagi-h-tech <mhkawsdd1027@gmail.com>
2024-02-17 16:04:34 +0900

    Arrayとアロー関数 foreach

--- main.js
+++ main.js
@@ -8,6 +8,23 @@ console.log(fruits);
 fruits.push('みかん');
 console.log(fruits);
 
+// callback 折り返し電話
 fruits.forEach(function (input){
   console.log(input);
 });
+
+// 一般的な関数の作り方
+// function getItem (){}
+
+// コールバック関数 名前がない関数（無名関数・匿名関数）
+
+// ES6 アロー関数 =>
+// 関数の名前あり
+// const getItem = () => {console.log('アロー')};
+const getItem = () => console.log('アロー');
+getItem();
+
+
+// 関数の名前なし
+
+fruits.forEach(input => console.log(input));
